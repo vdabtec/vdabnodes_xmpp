f17e65733479f6df20e6159f28e420f920124e22
markjapp <markjapp@gmail.com>
2018-10-18 06:56:21 -0400

    Added Apache License

--- XmppNodes/src/main/java/vdab/extnodes/xmpp/AFXmppMsg.java
+++ XmppNodes/src/main/java/vdab/extnodes/xmpp/AFXmppMsg.java
@@ -1,3 +1,18 @@
+/*LICENSE*
+ * Copyright (C) 2013 - 2018 MJA Technology LLC 
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 package vdab.extnodes.xmpp;
 
 import com.lcrc.af.AnalysisCompoundData;
@@ -33,7 +48,7 @@ public class AFXmppMsg extends AF_ADType_A{
 		return acd;			
 	}
 	@Override
-	public AnalysisData getExpandedData() {
+	public AnalysisData getExpandedData(String label) {
 		return getAnalysisData();
 	}
 }
